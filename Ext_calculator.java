264690b99c45f64ff19fc529a88bdf287da6acd8
sachin <01fe22bca023@kletech.ac.in>
2024-03-28 20:55:19 +0530

    add two files

---
+++ Ext_calculator.java
@@ -0,0 +1,17 @@
+public class Ext_calculator {
+
+    static float square (float x){
+    
+            return (x*x);
+    
+        }
+    
+    static float cube (float x){
+    
+            return (x*x*x);
+    
+        }
+    
+    }
+    
+    
